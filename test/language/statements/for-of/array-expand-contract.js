f7d07b4645f934063c9f71bc01256836eda00668
Mike Pennisi <mike@mikepennisi.com>
2015-05-27 18:39:03 -0400

    Add tests for `for..of` iteration over built-ins

---
+++ test/language/statements/for-of/array-expand-contract.js
@@ -0,0 +1,23 @@
+// Copyright (C) 2015 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: >
+    New entries inserted into an Array instance during traversal should not be
+    visited if they are removed prior to visitation.
+es6id: 13.6.4
+---*/
+
+var array = [0];
+var iterationCount = 0;
+
+for (var x of array) {
+  assert.sameValue(x, 0);
+
+  array.push(1);
+  array.pop();
+
+  iterationCount += 1;
+}
+
+assert.sameValue(iterationCount, 1);
---
+++ test/language/statements/for-of/array-key-get-error.js
@@ -0,0 +1,26 @@
+// Copyright (C) 2015 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: >
+    If retrieving an element from the array produces an error, that error
+    should be forwarded to the run time.
+es6id: 13.6.4
+---*/
+
+var array = [];
+var iterationCount = 0;
+
+Object.defineProperty(array, '0', {
+  get: function() {
+    throw new Test262Error();
+  }
+});
+
+assert.throws(Test262Error, function() {
+  for (var value of array) {
+    iterationCount += 1;
+  }
+});
+
+assert.sameValue(iterationCount, 0, 'The loop body is not evaluated');
