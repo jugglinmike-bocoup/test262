6f0d4206d4a3d4485a727d0f13adb4191ccb6784
Mike Pennisi <mike@mikepennisi.com>
2015-07-23 11:15:05 -0400

    Finish tests for RegExp.prototype[@@match]
    Still need to test internal methods, though :/

--- test/built-ins/RegExp/prototype/Symbol.match/g-match-empty-coerce-lastindex-err.js
+++ test/built-ins/RegExp/prototype/Symbol.match/g-match-empty-coerce-lastindex-err.js
@@ -3,7 +3,7 @@
 
 /*---
 description: >
-    Behavior when error is thrown while coercing `0` property of match result
+    Behavior when error is thrown while coercing `lastIndex` of zero-width match
 es6id: 21.2.5.6
 info: >
     7. If global is false, then
@@ -15,21 +15,38 @@ info: >
           [...]
           iv. Else result is not null,
               1. Let matchStr be ToString(Get(result, "0")).
-              2. ReturnIfAbrupt(matchStr).
+              [...]
+              5. If matchStr is the empty String, then
+                 a. Let thisIndex be ToLength(Get(rx, "lastIndex")).
+                 b. ReturnIfAbrupt(thisIndex).
 features: [Symbol.match]
 ---*/
 
 var r = /./g;
+var nextMatch;
+
 r.exec = function() {
+  var thisMatch = nextMatch;
+  if (thisMatch === null) {
+    return null;
+  }
+  nextMatch = null;
   return {
-    0: {
-      toString: function() {
-        throw new Test262Error();
-      }
+    get 0() {
+      r.lastIndex = {
+        valueOf: function() {
+          throw new Test262Error();
+        }
+      };
+      return thisMatch;
     }
   };
 };
 
+nextMatch = 'a non-empty string';
+r[Symbol.match]('');
+
+nextMatch = '';
 assert.throws(Test262Error, function() {
   r[Symbol.match]('');
 });
