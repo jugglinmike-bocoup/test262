76a4d9a06eefe3a57c3b55ab08e18b7fa15246f2
Mike Pennisi <mike@mikepennisi.com>
2015-08-05 13:19:24 -0400

    fixup! PARTIAL: Add tests for well-known Symbol: @@split

---
+++ test/built-ins/RegExp/prototype/Symbol.split/str-set-lastindex-err.js
@@ -0,0 +1,29 @@
+// Copyright (C) 2015 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+es6id: 21.2.5.11
+description: >
+    Behavior when error thrown while setting `lastIndex` property of splitter
+info: >
+    [...]
+    24. Repeat, while q < size
+        a. Let setStatus be Set(splitter, "lastIndex", q, true).
+        b. ReturnIfAbrupt(setStatus).
+features: [Symbol.split, Symbol.species]
+---*/
+
+var obj = {
+  constructor: function() {}
+};
+obj.constructor[Symbol.species] = function() {
+  return {
+    set lastIndex(_) {
+      throw new Test262Error();
+    }
+  };
+};
+
+assert.throws(Test262Error, function() {
+  RegExp.prototype[Symbol.split].call(obj, 'a');
+});
